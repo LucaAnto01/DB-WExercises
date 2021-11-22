be167bf4380744951dcfcf33a1e215aa4ee0883e
LucaAnto01 <luca.antognarelli@gmail.com>
2021-11-22 18:33:29 +0100

    File creation DB

--- University/createDBUniversity.sql
+++ University/createDBUniversity.sql
@@ -5,14 +5,14 @@ USE university;
 CREATE TABLE teacher(
     code integer(5) AUTO_INCREMENT,
     firstname varchar(25),
-    lastname varchar(25)
+    lastname varchar(25),
     primary key (code)
 );
 
 CREATE TABLE department(
     code integer(5) AUTO_INCREMENT,
-    name varchar(50),
-    address varchar(50),
+    departmentName varchar(50),
+    departmentAddress varchar(50),
     telephone varchar(10),
     primary key (code)
 );
@@ -20,7 +20,7 @@ CREATE TABLE department(
 CREATE TABLE graduateCourse(
     code integer(5) AUTO_INCREMENT,
     fkCodeDepartment integer(5),
-    name varchar(50),
+    graduateCourseName varchar(50),
     primary key (code),
     FOREIGN KEY (fkCodeDepartment) REFERENCES department(code)
 );
@@ -31,10 +31,26 @@ CREATE TABLE students(
     firstname varchar(25),
     lastname varchar(25),
     birth date,
-    address varchar(50),
+    placeAddress varchar(50),
     telephone varchar(10),
     primary key (matricola),
     FOREIGN KEY (fkGraduateCourse) REFERENCES graduateCourse(code)
 );
 
-/*TODO: finish it*/
+CREATE TABLE course(
+    code integer(5) AUTO_INCREMENT,
+    fkCodeDepartment integer(5),
+    courseName varchar(50),
+    primary key (code),
+    FOREIGN KEY (fkCodeDepartment) REFERENCES department(code)
+);
+
+CREATE TABLE exams(
+    fkMatricola integer(6),
+    fkCourseCode integer(5),
+    dataExam date,
+    vote tinyint(2) CHECK(vote > 0 && vote <= 30),
+    honours enum('Y', 'N'),
+    FOREIGN KEY (fkMatricola) REFERENCES students(matricola),
+    FOREIGN KEY (fkCourseCode) REFERENCES course(code)
+);
