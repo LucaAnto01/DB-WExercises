f1b7bf713be6d595e72b327b53373a58dfc23ca7
LucaAnto01 <luca.antognarelli@gmail.com>
2021-11-25 17:40:55 +0100

    Complete exercise

--- TravelAgency/populateDb/dataTabClients.sql
+++ TravelAgency/populateDb/dataTabClients.sql
@@ -1,3 +1,3 @@
 INSERT INTO clients(username, birthdate, firstname, lastname) VALUES ('mariorossi99', '1999-01-11' ,'Mario', 'Rossi');
 INSERT INTO clients(username, birthdate, firstname, lastname) VALUES ('giuseppeverdi01', '2001-10-07' ,'Giuseppe', 'Verdi');
-INSERT INTO clients(username, birthdate, firstname, lastname) VALUES ('carlacarl77', '1977-09-11' ,'Carla', 'Carletta');
+INSERT INTO clients(username, birthdate, firstname, lastname) VALUES ('carlacarl77', '1977-09-11' ,'Franca', 'Carletta');
